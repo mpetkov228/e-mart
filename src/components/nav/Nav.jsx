b46cc6b4b4cf5564fbcd6ac48f7f35394c27ac71
MihailPetkov96 <mpetkov228@gmail.com>
2024-03-27 17:46:29 +0200

    Create navbar component

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+	return (
+		<div>
+			<div>E-MART</div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Cart</a></li>
+      </ul>
+		</div>
+	);
+
+}
+
+export default Nav;
