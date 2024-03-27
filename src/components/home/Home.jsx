08c3b7d70cd7dd0db34ce3e227d4ce00348bedaf
MihailPetkov96 <mpetkov228@gmail.com>
2024-03-27 22:38:46 +0200

    Add shop now button in home component

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 import "./Home.css";
 
 function Home() {
+  const navigate = useNavigate();
+
   return (
     <div className="home bg">
-      
+      <div className="callToActionHome">
+        <h1 className="homeHeading">New Inventory</h1>
+        <button onClick={() => navigate("shop")} className="shopNowBtn">Shop now</button>
+      </div>
     </div>
   );
 }
