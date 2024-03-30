dd107b0e880797bcd5af8f2463e24bb75c56fdf3
MihailPetkov96 <mpetkov228@gmail.com>
2024-03-30 22:34:44 +0200

    Add styles to categories component

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -3,8 +3,8 @@ import "./Categories.css";
 function Categories() {
   return (
     <div className="categories">
-      <h2>Categories</h2>
-      <ul>
+      <h1>Categories</h1>
+      <ul className="categoriesList">
         <li>Men's clothing</li>
         <li>Women's clothing</li>
         <li>Jewelery</li>
